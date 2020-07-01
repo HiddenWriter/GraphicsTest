e994f0ac05d9f2a649ba6dc1c5f7ecac60404db0
HiddenWriter <shinonome96@gmail.com>
2020-07-01 18:21:13 +0900

    Add Message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main (void) {
-    std::cout << "\n\t Test Message . 'n";
+    std::cout << "\n\t Test Message . \n";
+    std::cout << "\n\t Test Test Test . \n";
     return 0;
 }
